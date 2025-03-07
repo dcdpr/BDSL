3f7e27fd7421e78cb2341aeff608114af08c6684
Kevin Dean <kevin@legreq.com>
2025-03-07 10:54:02 -0500

    Added missing configuration parameter.

--- crates/butter/build.rs
+++ crates/butter/build.rs
@@ -1,6 +1,6 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("cargo:rerun-if-changed=assets/design_tokens.json");
-    dtoken::build("assets/design_tokens.json")?;
+    dtoken::build("assets/design_tokens.json", Config::default())?;
 
     Ok(())
 }
